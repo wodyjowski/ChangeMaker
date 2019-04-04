1491a89252ed7873a9415c2c614544096ccc4780
Michał Kisiel <mkisiel58@gmail.com>
2019-04-04 22:57:04 +0200

    Amount txtbox binding and input validation

--- ChangeMaker/MainWindow.xaml.cs
+++ ChangeMaker/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,10 +21,25 @@ namespace ChangeMaker
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ViewModel viewModel = new ViewModel();
         public MainWindow()
         {
             InitializeComponent();
+            textBoxAmount.DataContext = viewModel;
         }
 
+        private void RichTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            richTextOutput.AppendText(viewModel.Amount);
+        }
+
+        private void TextBoxAmount_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !Regex.IsMatch(e.Text, @"\d");
+        }
     }
 }
